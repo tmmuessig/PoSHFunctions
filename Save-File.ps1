1515a63a0e6ced3ff0ae701cc054ab4a44259612
Tim Muessig <tmmuessig@gmail.com>
2021-08-19 13:07:34 -0500

    Initial commit

---
+++ Save-File.ps1
@@ -0,0 +1,34 @@
+﻿Function Save-File
+{
+    [CmdletBinding()]
+    Param
+    (
+        [String]
+        $Path = "C:\"
+    )
+    Begin
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+    }
+
+    Process
+    {
+        $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+        if (Test-Path $Path)
+        {
+            $OpenFileDialog.initialDirectory = $Path
+        }
+        Else
+        {
+            $OpenFileDialog.initialDirectory = "C:\"
+        }
+        $OpenFileDialog.Filter = "All files (*.*)| *.*"
+        $OpenFileDialog.ShowDialog() | Out-Null
+
+    }
+
+    End
+    {
+        Return $OpenFileDialog.FileName
+    }
+}
---
+++ Show-MessageBox.ps1
@@ -0,0 +1,43 @@
+﻿Function Show-MessageBox
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter()]
+        [String]
+        $Title,
+
+        [Parameter()]
+        [String]
+        $Message,
+
+        [Parameter()]
+        # [Enum]::GetNames([System.Windows.Forms.MessageBoxButtons])
+        [ValidateSet('OK', 'OKCancel', 'AbortRetryIgnore', 'YesNoCancel', 'YesNo', 'RetryCancel')]
+        [String]
+        $Button = 'OK',
+
+        [Parameter()]
+        # [Enum]::GetNames([System.Windows.Forms.MessageBoxIcon])
+        [ValidateSet('None', 'Hand', 'Error', 'Stop', 'Question', 'Exclamation', 'Warning', 'Asterisk', 'Information')]
+        [String]
+        $Icon = 'Information'
+    )
+
+    Begin
+    {
+        #add the type so we can access Windows.Forms
+        Add-Type -AssemblyName System.Windows.Forms
+    }
+
+    Process
+    {
+        # The items in the Show must be in this specific order
+        [System.Windows.Forms.MessageBox]::Show($Message, $Title, $Button, $Icon)
+    }
+
+    End
+    {
+
+    }
+}
