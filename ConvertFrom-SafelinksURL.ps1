015e0ccd2c9205cbbf28537be8d30081d7d41acd
Tim Muessig <tmmuessig@gmail.com>
2022-01-18 08:31:37 -0600

    Added ConvertFrom-SafeLinks function

---
+++ ConvertFrom-SafelinksURL.ps1
@@ -0,0 +1,29 @@
+﻿Function ConvertFrom-SafelinksURL
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter()]
+        [String[]]
+        $SafeLinkURL
+    )
+
+    Begin
+    {
+        [System.Collections.ArrayList]$ConvertedURLs = @()
+    }
+
+    Process
+    {
+        Foreach ($URL in $SafeLinkURL)
+        {
+            $URL = (($URL.Remove(0, 52) -replace '%3A', ':' -replace '%2F', "/") -split "&")[0] 
+            [void]$ConvertedURLs.Add($URL)
+        }
+    }
+
+    End
+    {
+        $ConvertedURLs
+    }
+}
