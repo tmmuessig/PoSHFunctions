6143e25ade607e7da24dc54c6d3738687af2d756
Tim Muessig <tmmuessig@gmail.com>
2022-07-01 10:06:43 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,47 @@
-﻿#Jumping off point for a PowerShell Console Profile
+################## Load Functions ##################
+function New-CommentBox () {
+    param([string]$titleText)
+    $lines = $titleText.Split("`n")
+    $output = "$("#"*70)`n"
+    $output += "#$(" "*68)#`n"
 
-#Ensure the console is set to desired size
-$Width = 160
-$Height = 55
-$Host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size($Width, 3000)
-$Host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size($Width, $Height)
+    foreach($line in $lines){
+        if($line.Length -gt 65){
+            $line = $line.Substring(0,66)
+        }
+        $line = $line.Trim()
+        $lspaces = ([math]::Floor((68-$line.trim().Length)/2))
+        $rspaces = (68-$lspaces-$line.Length)
+        $output += "#$(" "*$lspaces)$($line.trim())$(" "*$rspaces)#`n"
+
+    }
+    $output += "#$(" "*68)#`n"
+    $output += "$("#"*70)`n"
+    return $output
+}
+
+################## Set Theme Options ##################
+#Powerline setup https://docs.microsoft.com/en-us/windows/terminal/tutorials/powerline-setup
+
+If ((Get-Host).Name -ne 'Visual Studio Code Host') {
+    Import-Module posh-git
+    Import-Module oh-my-posh
+    Import-Module -Name Terminal-Icons
+    Set-PoshPrompt powerlevel10k_rainbow
+    #Set-PoshPrompt C:\Users\timuessi\Documents\PowerShell\Modules\oh-my-posh\themes\My-Aliens.omp.json
+    #Set-PoshPrompt C:\Users\timuessi\Documents\PowerShell\Modules\oh-my-posh\themes\blue-owl.omp.json
+    #Set-PoshPrompt -Theme Blue-Owl #Aliens #Paradox
+}
+
+################## Set PSReadline Options ##################
+Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
+Set-PSReadlineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView  # InLineView  or ListView
+Set-PSReadLineOption -Colors @{ InLinePrediction = "`e[36;7;238m" }
 
 Write-Host "PowerShell "  -ForegroundColor Green -noNewLine
 Write-Host "$($PSVersionTable.PSEdition)"  -ForegroundColor Red -NoNewLine
 
-#Powerline setup https://docs.microsoft.com/en-us/windows/terminal/tutorials/powerline-setup
+Write-Host " "
 
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
-
-If (-not($PSVersionTable.PSVersion -like "7.2*"))
-{
-    import-module az.tools.predictor # Used for the PredictionSource plugin
-}
-
-
-Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
-Set-PSReadlineOption -BellStyle None
-Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView # InLineView  or ListView
-Set-PSReadLineOption -Colors @{ InLinePrediction = "`e[36;7;238m" }
+Set-Location C:\
